732cb35a141c8bdb8654490893a84339f621fd91
Abhishektrivedi295 <abhishek26699469@gmail.com>
2026-05-23 14:01:09 +0530

    frontend initial commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+          <span>All rights reserve 2026 by Abhishek </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
