753dcb8b7ef4621a4381ebe1825c8f19a5ce1a56
Abhishektrivedi295 <abhishek26699469@gmail.com>
2026-05-24 14:31:08 +0530

    updated live api

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios, { Axios } from "axios";
 
-const REST_API_BASE_URL = "https://ems-backend-production-eef9.up.railway.app";
+const REST_API_BASE_URL = "https://ems-backend-production-eef9.up.railway.app/api/employees";
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
